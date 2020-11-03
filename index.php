cbbb512ccffc61f92aecb484489c6b6d3012d738
71318620 <71318620+jartsajartsa@users.noreply.github.com>
2020-11-03 10:06:51 +0200

    joo

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>salattusivuprojekti</title>
+    <link rel="stylesheet" href="css/style.css">
+    </head>
