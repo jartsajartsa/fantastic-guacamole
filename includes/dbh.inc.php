c5daed6316acbb4ba5a7d42f0c7dc119e9bc893c
71318620 <71318620+jartsajartsa@users.noreply.github.com>
2020-11-18 09:50:23 +0200

    testiätestiä

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,21 +1,51 @@
 <?php
 
+// $dBName = "limeanteri";
+
+// include('../../tunnukset.php');
+
+// $local = in_array($_SERVER['REMOTE_ADDR'],array('127.0.0.1','REMOTE_ADDR' => '::1'));
+
+// if (!$local) {
+//   $password = $db_pwd_azure;
+//   $user = $db_username_azure;
+//   $server = "localhost:49492";
+//   }
+// else {
+//   $password = $db_pwd_local;
+//   $user = $db_username_local;
+//   $server = "localhost";	
+//   }
+
+
+// $link = mysqli_connect($server,$user,$password,$dBName);
+
+// if(!$link) {
+//     if (!$link) {
+//         die("Connection failed: " . mysqli_connect_error());       
+//     }
+// }
+
+?>
+
+<?php
+
 $dBName = "limeanteri";
 
 include('../../tunnukset.php');
 
-$local = in_array($_SERVER['REMOTE_ADDR'],array('127.0.0.1','REMOTE_ADDR' => '::1'));
+// $local = in_array($_SERVER['REMOTE_ADDR'],array('127.0.0.1','REMOTE_ADDR' => '::1'));
 
-if (!$local) {
+// if (!$local) {
   $password = $db_pwd_azure;
   $user = $db_username_azure;
   $server = "localhost:49492";
-  }
-else {
-  $password = $db_pwd_local;
-  $user = $db_username_local;
-  $server = "localhost";	
-  }
+  // }
+// else {
+//   $password = $db_pwd_local;
+//   $user = $db_username_local;
+//   $server = "localhost";	
+//   }
 
 
 $link = mysqli_connect($server,$user,$password,$dBName);
