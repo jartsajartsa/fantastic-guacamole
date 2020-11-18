b5ad21a81adc67202f357e71f0ab4435f671a2ea
71318620 <71318620+jartsajartsa@users.noreply.github.com>
2020-11-18 10:03:30 +0200

    lisää

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -18,18 +18,13 @@ $local = in_array($_SERVER['REMOTE_ADDR'],array('127.0.0.1','REMOTE_ADDR' => '::
 if (!$local) {
   $password = $db_pwd_azure;
   $user = $db_username_azure;
-  $server = "localhost:49492";
+  $server = "localhost:8374877";
   }
 else {
   $password = $db_pwd_local;
   $user = $db_username_local;
-  $server = "localhost";	
+  $server = "localhost";
   }
-
-  echo($password);
-  echo($user);
-  echo($server);
-  echo($dBName);
 
 $link = mysqli_connect($server,$user,$password,$dBName);
 
